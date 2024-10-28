576ad2cefbe311bf75f679d82bf516a8e5d42767
thefirstSnowflake <86607684+thefirstSnowflake@users.noreply.github.com>
2024-10-28 15:46:58 +0400

    Update material_practice.dart

--- material_practice.dart
+++ material_practice.dart
@@ -29,34 +29,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  PersistentBottomSheetController? _controller;
+
+  void openDrawer() {
+    scaffoldKey.currentState!.openEndDrawer();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+
       appBar: AppBar(
           title: Text('Hello, Flutter app!'),
           backgroundColor: Color.fromARGB(255, 159, 196, 239),
+          actions: [
+            Builder(builder: (context) => IconButton(
+            onPressed: () {
+              Scaffold.of(context).openEndDrawer();
+            }, 
+            icon: Icon(Icons.account_circle_rounded)),
+            )
+          ],
          ),
       
       bottomNavigationBar: BottomAppBar(
         child: Container(
           child: BottomNavigationBar(
-            elevation: 0,
               items: [
+              
               BottomNavigationBarItem(
                 icon: Icon(Icons.home),
-                label: "Home",
-                
+                label: "Home", 
               ),
+              
               BottomNavigationBarItem(
                 icon: Icon(Icons.chat),
                 label: "Chat",
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.album),
-                label: "Albums",
-              ),
+              
             ],
-            ),
+          ),
         ),
       ),
 
@@ -66,40 +80,55 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: () {},
       ),
 
+      endDrawer: Drawer(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [ 
+              CircleAvatar(
+                backgroundImage: NetworkImage('https://loremflickr.com/320/240/cat'),
+                radius: 50,
+              ),
+              Text('username'),
+            ]
+          )
+      ),
+
       drawer: Drawer(
           child: Column(children: [
-        DrawerHeader(
-          child: CircleAvatar(
-            backgroundImage: NetworkImage(
-                'https://i.pinimg.com/736x/81/df/db/81dfdbe10de7eacdb6ddca47c7cc9181.jpg'),
-            radius: 60,
+          DrawerHeader(
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://i.pinimg.com/736x/81/df/db/81dfdbe10de7eacdb6ddca47c7cc9181.jpg'),
+              radius: 60,
+            ),
           ),
-        ),
-        ListTile(
-          title: Text('Home'),
-          leading: Icon(Icons.home),
-          trailing: Icon(Icons.chevron_right),
-        ),
-        ListTile(
-          title: Text('Profile'),
-          leading: Icon(Icons.person_3_rounded),
-          trailing: Icon(Icons.chevron_right),
-        ),
-        ListTile(
-          title: Text('Images'),
-          leading: Icon(Icons.image),
-          trailing: Icon(Icons.chevron_right),
-        ),
-        Expanded(
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                TextButton(onPressed: () {}, child: Text('Выход')),
-                TextButton(onPressed: () {}, child: Text('Регистрация')),
-              ]),
-        )
-      ])),
+          ListTile(
+            title: Text('Home'),
+            leading: Icon(Icons.home),
+            trailing: Icon(Icons.chevron_right),
+          ),
+          ListTile(
+            title: Text('Profile'),
+            leading: Icon(Icons.person_3_rounded),
+            trailing: Icon(Icons.chevron_right),
+          ),
+          ListTile(
+            title: Text('Images'),
+            leading: Icon(Icons.image),
+            trailing: Icon(Icons.chevron_right),
+          ),
+          Expanded(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextButton(onPressed: () {}, child: Text('Выход')),
+                  TextButton(onPressed: () {}, child: Text('Регистрация')),
+                ]),
+          )
+        ])),
     ); // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
