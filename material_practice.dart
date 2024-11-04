73f67d8676e6db65191c4197aa8d27bc9eb6c8b0
thefirstSnowflake <86607684+thefirstSnowflake@users.noreply.github.com>
2024-11-04 20:54:03 +0400

    Update material_practice_5_5.dart
    Ready Material/Cupertino practice 5.5

--- material_practice.dart
+++ material_practice.dart
@@ -6,14 +6,14 @@ void main() {
 
 class TabItem {
   String? title;
-  Icon? icon;
-  TabItem({this.title, this.icon});
+  Icon icon;
+  TabItem({this.title, required this.icon});
 }
 
 final List<TabItem> _tabBar = [
-  TabItem(title: "Home", icon: Icon(Icons.home)),
-  TabItem(title: "Chat", icon: Icon(Icons.chat)),
-  TabItem(title: "Albums", icon: Icon(Icons.album))
+  TabItem(title: "Home", icon: const Icon(Icons.home, size: 22)),
+  TabItem(title: "Chat", icon: const Icon(Icons.chat, size: 22)),
+  TabItem(title: "Albums", icon: const Icon(Icons.album, size: 22))
 ];
 
 class MyApp extends StatelessWidget {
@@ -44,47 +44,66 @@ class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   TabController? _tabController;
   int _currentTabIndex = 0;
-  final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   PersistentBottomSheetController? _controller;
 
-  void toggleBottomSheet() {
-    if (_controller == null) {
-      _controller =
-          scaffoldKey.currentState!.showBottomSheet((context) => Container(
-                child: Row(children: [
+  void toggleBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) => SafeArea(
+                child: Container(
+              height: 150,
+              child: Column(children: [
+                const SizedBox(height: 20),
+                Row(children: [
+                  const SizedBox(width: 20),
                   Icon(Icons.credit_card_outlined, size: 22),
+                  const SizedBox(width: 40),
                   Text("Сумма"),
-                  Text("200 руб"),ф
+                  const SizedBox(width: 200),
+                  Text("200 руб"),
                 ]),
-                color: Color.fromARGB(148, 226, 226, 226),
-                height: 150,
-              ));
-    } else {
-      _controller!.close();
-      _controller = null;
-    }
+                const SizedBox(height: 15),
+                TextButton(
+                    onPressed: () {},
+                    child: const Text("Оплатить"),
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all<Color>(
+                          Color.fromARGB(255, 159, 196, 239)),
+                      foregroundColor: WidgetStateProperty.all<Color>(
+                          Color.fromARGB(255, 59, 30, 67)),
+                    )),
+              ]),
+            )));
   }
 
   @override
   void initState() {
-    _tabController = TabController(length: _tabBar.length, vsync: this);
     super.initState();
+    _tabController = TabController(length: _tabBar.length, vsync: this);
+
+    _tabController!.addListener(() {
+      setState(() {
+        print('Listener ${_tabController!.index}');
+        _currentTabIndex = _tabController!.index;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: scaffoldKey,
+      //  key: scaffoldKey,
       appBar: AppBar(
-        title: Text('Hello, Flutter app!'),
-        backgroundColor: Color.fromARGB(255, 159, 196, 239),
+        title: const Text('Hello, Flutter app!'),
+        backgroundColor: const Color.fromARGB(255, 159, 196, 239),
         actions: [
           Builder(
             builder: (context) => IconButton(
                 onPressed: () {
                   Scaffold.of(context).openEndDrawer();
                 },
-                icon: Icon(Icons.account_circle_rounded)),
+                icon: const Icon(Icons.account_circle_rounded)),
           )
         ],
       ),
@@ -103,34 +122,34 @@ class _MyHomePageState extends State<MyHomePage>
       ),
       floatingActionButton: SafeArea(
         child: FloatingActionButton(
+          backgroundColor: const Color.fromARGB(255, 239, 187, 255),
+          onPressed: () => toggleBottomSheet(context),
           child: Icon(Icons.add),
-          backgroundColor: Color.fromARGB(255, 239, 187, 255),
-          onPressed: toggleBottomSheet,
         ),
       ),
       bottomNavigationBar: SafeArea(
         child: BottomAppBar(
           child: Container(
             child: BottomNavigationBar(
+              onTap: (index) {
+                setState(() {
+                  _tabController!.index = index;
+                  _currentTabIndex = index;
+                });
+              },
+              currentIndex: _currentTabIndex,
               items: [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home, size: 22),
-                  label: "Home",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.chat, size: 22),
-                  label: "Chat",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.album, size: 22),
-                  label: "Albums",
-                ),
+                for (final item in _tabBar)
+                  BottomNavigationBarItem(
+                    label: item.title,
+                    icon: item.icon,
+                  )
               ],
             ),
           ),
         ),
       ),
-      endDrawer: SafeArea(
+      endDrawer: const SafeArea(
         child: Drawer(
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -147,24 +166,24 @@ class _MyHomePageState extends State<MyHomePage>
       drawer: SafeArea(
         child: Drawer(
             child: Column(children: [
-          DrawerHeader(
+          const DrawerHeader(
             child: CircleAvatar(
               backgroundImage: NetworkImage(
                   'https://i.pinimg.com/736x/81/df/db/81dfdbe10de7eacdb6ddca47c7cc9181.jpg'),
               radius: 60,
             ),
           ),
-          ListTile(
+          const ListTile(
             title: Text('Home'),
             leading: Icon(Icons.home),
             trailing: Icon(Icons.chevron_right),
           ),
-          ListTile(
+          const ListTile(
             title: Text('Profile'),
             leading: Icon(Icons.person_3_rounded),
             trailing: Icon(Icons.chevron_right),
           ),
-          ListTile(
+          const ListTile(
             title: Text('Images'),
             leading: Icon(Icons.image),
             trailing: Icon(Icons.chevron_right),
@@ -174,8 +193,24 @@ class _MyHomePageState extends State<MyHomePage>
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  TextButton(onPressed: () {}, child: Text('Выход')),
-                  TextButton(onPressed: () {}, child: Text('Регистрация')),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text('Выход'),
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color>(
+                            Color.fromARGB(255, 159, 196, 239)),
+                        foregroundColor: WidgetStateProperty.all<Color>(
+                            Color.fromARGB(255, 59, 30, 67)),
+                      )),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text('Регистрация'),
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color>(
+                            Color.fromARGB(255, 159, 196, 239)),
+                        foregroundColor: WidgetStateProperty.all<Color>(
+                            Color.fromARGB(255, 59, 30, 67)),
+                      )),
                 ]),
           )
         ])),
