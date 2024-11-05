6e47875f1a6e24896d4ec6d1dea3afdc6c8c3dc1
thefirstSnowflake <86607684+thefirstSnowflake@users.noreply.github.com>
2024-11-05 20:49:29 +0400

    Update №4_practice_6_5.dart

--- №4_practice_6_5.dart
+++ №4_practice_6_5.dart
@@ -31,35 +31,52 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   //final List <int> sequenceOfNumbers = [for (var i = 0; i <= 100; i++) i]; //генерируем список чисел от 0 до 100
 
-   @override
+  @override
   void initState() {
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      body: SafeArea( 
-        child: Container( 
-          margin: EdgeInsets.only(right: 10),
-          child: 
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Search",
-                //hintText: "Search",
-                
-                prefixIcon: Icon(Icons.search, size: 22),
-                
-                helperText: "Поле для поиска заметок",
-                iconColor: Color.fromARGB(255, 187, 0, 255)
-              )
-        )
-        
-        )
-      )
-    );
+        body: SafeArea(
+            //чтобы не входить за границы приложения
+            child: Align(
+                //выравнивание
+                alignment: Alignment.topCenter, //по центру
+                child: Container(
+                    width: 350, //ширина инпута = 350
+                    margin: EdgeInsets.only(top: 20), //отступ сверху = 20
+                    child: TextField(
+                        //инициализируем текстовое поле для ввода
+                        decoration: InputDecoration(
+                      //создаем для него оформление
+                      enabledBorder: OutlineInputBorder(
+                          //оформление для границ поля в пассивном состоянии
+                          borderRadius: BorderRadius.circular(
+                              15), //радиус границы = 15 (скругленная)
+                          borderSide: BorderSide(
+                              //цвет границы
+                              color: Color.fromARGB(255, 187, 0, 255))),
+                      focusedBorder: OutlineInputBorder(
+                          //оформление для границ поля в состоянии выбора и ввода
+                          borderRadius: BorderRadius.circular(
+                              15), //радиус границы = 15 (скругленная)
+                          borderSide: BorderSide(
+                              //цвет границы
+                              color: Color.fromARGB(255, 187, 0, 255))),
+                      labelText: "Search", //надпись по умолчанию и сверху
+                      labelStyle: //цвет надписи
+                          TextStyle(color: Color.fromARGB(255, 187, 0, 255)),
+                      hintText: "Введите значение", //подсказка
+                      hintStyle: TextStyle(
+                          color: Color.fromARGB(
+                              255, 183, 183, 183), //цвет подсказки и размер
+                          fontSize: 15),
+                      suffixIcon: Icon(Icons.search, size: 22), //иконка справа
+                      suffixIconColor:
+                          Color.fromARGB(255, 187, 0, 255), //цвет иконки
+                      helperText: "Поле для поиска заметок", //надпись внизу
+                    ))))));
   }
 }
-
