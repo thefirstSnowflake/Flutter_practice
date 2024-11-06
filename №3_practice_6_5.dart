290856a231b4f5ad3f1d98af2150567a0220fa36
thefirstSnowflake <86607684+thefirstSnowflake@users.noreply.github.com>
2024-11-06 12:20:37 +0400

    Update №3_practice_6_5.dart
    Skillbox Flutter. Practice 6.5 (Layout), exercise 3 (ListView, Layout Builder)

--- №3_practice_6_5.dart
+++ №3_practice_6_5.dart
@@ -4,15 +4,7 @@ void main() {
   runApp(const MyApp());
 }
 
-final fakeData = (int count) => List<Widget>.generate(
-      count,
-      (i) {
-        return Card(
-          color: Colors.green,
-        );
-      },
-    );
-final cards = fakeData(100);
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -38,11 +30,26 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final List<int> sequenceOfNumbers = [
-    for (var i = 0; i <= 100; i++) i
-  ]; //генерируем список чисел от 0 до 100
 
+class SimpleCard extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Container(
+      child: Card(
+        color: Color.fromARGB(255, 82, 234, 156),
+        child: ListTile(),
+      )
+    );
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Widget> data = List.generate(50, (index){
+    return SimpleCard();
+  });
+  
   @override
   void initState() {
     super.initState();
@@ -51,54 +58,28 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Container(
-        height: 100,
-        child: ListView.builder(
-          scrollDirection: Axis.vertical,
-          itemCount: cards.length,
-          itemBuilder: (context, index) => cards[index],
-        ),
+      body: SafeArea(child: 
+      LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints){
+        double _width = constraints.constrainWidth();
+        return _width > 500
+          ? Center(child: Container( 
+              padding: EdgeInsets.only(left: 50, right: 50),
+              child: ListView(
+              scrollDirection: Axis.vertical,
+              children: <Widget>[...data.map((item) => Container(width: 100, child: item)).toList()],
+              )
+            )
+          )
+          : Container( 
+              height: 100,
+              child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[...data.map((item) => Container(width: 100, child: item)).toList()],
+              )
+            );
+        },
       ),
-      /* body: SafeArea(
-            //чтобы не входить за границы приложения
-            child: Align(
-                //выравнивание
-                alignment: Alignment.topCenter, //по центру
-                child: Container(
-                    width: 350, //ширина инпута = 350
-                    margin: EdgeInsets.only(top: 20), //отступ сверху = 20
-                    child: TextField(
-                        //инициализируем текстовое поле для ввода
-                        decoration: InputDecoration(
-                      //создаем для него оформление
-                      enabledBorder: OutlineInputBorder(
-                          //оформление для границ поля в пассивном состоянии
-                          borderRadius: BorderRadius.circular(
-                              15), //радиус границы = 15 (скругленная)
-                          borderSide: BorderSide(
-                              //цвет границы
-                              color: Color.fromARGB(255, 187, 0, 255))),
-                      focusedBorder: OutlineInputBorder(
-                          //оформление для границ поля в состоянии выбора и ввода
-                          borderRadius: BorderRadius.circular(
-                              15), //радиус границы = 15 (скругленная)
-                          borderSide: BorderSide(
-                              //цвет границы
-                              color: Color.fromARGB(255, 187, 0, 255))),
-                      labelText: "Search", //надпись по умолчанию и сверху
-                      labelStyle: //цвет надписи
-                          TextStyle(color: Color.fromARGB(255, 187, 0, 255)),
-                      hintText: "Введите значение", //подсказка
-                      hintStyle: TextStyle(
-                          color: Color.fromARGB(
-                              255, 183, 183, 183), //цвет подсказки и размер
-                          fontSize: 15),
-                      suffixIcon: Icon(Icons.search, size: 22), //иконка справа
-                      suffixIconColor:
-                          Color.fromARGB(255, 187, 0, 255), //цвет иконки
-                      helperText: "Поле для поиска заметок", //надпись внизу
-                    ))))));*/
-    ));
+      )
+    );
   }
 }
