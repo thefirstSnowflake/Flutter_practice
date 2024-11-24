1d56ac5ac5150c9baa048b6c468f4ef0e322608e
thefirstSnowflake <86607684+thefirstSnowflake@users.noreply.github.com>
2024-11-24 22:22:02 +0400

    Update async_practice_8_7.dart

--- async_practice_8_7.dart
+++ async_practice_8_7.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-  
+
   final String title;
 
   @override
@@ -34,25 +34,21 @@ class _MyHomePageState extends State<MyHomePage> {
   final fileNameController = TextEditingController();
   bool showText = false;
   @override
-  void dispose(){
+  void dispose() {
     fileNameController.dispose();
     super.dispose();
   }
 
- 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 170, 220, 255),
-        title: Text('Hello! Let' 's read the files!'),
-      ),
-      body: SafeArea(
-        child: Container(
-
-          width: 350, //ширина инпута = 350
-          margin: EdgeInsets.only(top: 20, left: 20),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 170, 220, 255),
+          title: Text('Hello! Let' 's read the files!'),
+        ),
+        body: SafeArea(
+            child: Container(
+          margin: EdgeInsets.only(top: 20, left: 15, right: 15),
           child: Column(
             children: [
               Row(children: [
@@ -63,16 +59,24 @@ class _MyHomePageState extends State<MyHomePage> {
                       //создаем для него оформление
                       enabledBorder: OutlineInputBorder(
                           //оформление для границ поля в пассивном состоянии
-                          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10.0), topLeft: Radius.circular(10.0) ), //радиус границы = 15 (скругленная)
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10.0),
+                              topLeft: Radius.circular(
+                                  10.0)), //радиус границы = 15 (скругленная)
                           borderSide: BorderSide(
                               //цвет границы
-                              color: Color.fromARGB(255, 0, 0, 0), width: 2)),
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              width: 2)),
                       focusedBorder: OutlineInputBorder(
                           //оформление для границ поля в пассивном состоянии
-                          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10.0), topLeft: Radius.circular(10.0) ), //радиус границы = 15 (скругленная)
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10.0),
+                              topLeft: Radius.circular(
+                                  10.0)), //радиус границы = 15 (скругленная)
                           borderSide: BorderSide(
                               //цвет границы
-                              color: Color.fromARGB(255, 0, 0, 0), width: 2)),
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              width: 2)),
                       hintText: "Введите имя файла", //подсказка
                       hintStyle: TextStyle(
                           color: Color.fromARGB(
@@ -82,68 +86,68 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 SizedBox(
-                        height: 60,
-                        width: 100,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            setState(() { //переключить условие на true, чтобы разрешить отображение текста из FutureBuilder
-                              showText = true;
-                            });
-                          },
-                          style: ButtonStyle(
-                            shape:
-                                WidgetStateProperty.all<RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.only(
-                                            bottomLeft: Radius.circular(0.0),
-                                            bottomRight: Radius.circular(10.0),
-                                            topLeft: Radius.circular(0.0),
-                                            topRight: Radius.circular(10.0)))),
-                            backgroundColor: WidgetStateProperty.all<Color>(
-                                Color.fromARGB(255, 0, 0, 0)),
-                            foregroundColor: WidgetStateProperty.all<Color>(
-                                Color.fromARGB(255, 255, 255, 255)),
-                          ),
-                          child: Text('Найти'),
-                        )
-                ),
+                    height: 56,
+                    width: 100,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          //переключить условие на true, чтобы разрешить отображение текста из FutureBuilder
+                          showText = true;
+                        });
+                      },
+                      style: ButtonStyle(
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(0.0),
+                                    bottomRight: Radius.circular(10.0),
+                                    topLeft: Radius.circular(0.0),
+                                    topRight: Radius.circular(10.0)))),
+                        backgroundColor: WidgetStateProperty.all<Color>(
+                            Color.fromARGB(255, 0, 0, 0)),
+                        foregroundColor: WidgetStateProperty.all<Color>(
+                            Color.fromARGB(255, 255, 255, 255)),
+                      ),
+                      child: Text('Найти'),
+                    )),
               ]),
+              (showText == true)
+                  ? Column(children: [
+                      Container(
+                        padding: EdgeInsets.only(top: 15, bottom: 15),
+                        alignment: Alignment.topLeft,
+                        child: Text(fileNameController.text + ' file'),
+                      ),
+                      FutureBuilder<String>(
+                          future: fetchFileFromAssets(fileNameController.text),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<String> snapshot) {
+                            print(snapshot.data);
+                            switch (snapshot.connectionState) {
+                              case ConnectionState.waiting:
+                                return Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              case ConnectionState.done:
+                                if (snapshot.data == null) {
+                                  return Container(
+                                      alignment: Alignment.topLeft,
+                                      child: Text('The file was not found.'));
+                                }
+                                return Container(
+                                    padding: EdgeInsets.only(left: 10),
+                                    alignment: Alignment.topLeft,
+                                    child: Text(snapshot.data!));
+                              default:
+                                return Center(
+                                    child: CircularProgressIndicator());
+                            }
+                          })
+                    ])
+                  : const SizedBox(),
+            ],
           ),
-              (showText == true) 
-              ? Column(
-                
-                children: [
-                Container(
-                  padding: EdgeInsets.only(top: 15, bottom: 15),
-                  alignment: Alignment.topLeft,
-                child: Text(fileNameController.text + ' file'), 
-                ),
-               
-                
-               FutureBuilder<String>(
-                future: fetchFileFromAssets(fileNameController.text), 
-                builder: (BuildContext context, AsyncSnapshot<String> snapshot){
-                print(snapshot.data);
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                    return Center(child: CircularProgressIndicator(),
-                );
-                  case ConnectionState.done:
-                    if (snapshot.data == null) {
-                      return Container( padding: EdgeInsets.only(left: 10), alignment: Alignment.topLeft, child: Text('The file was not found.'));
-                  }
-                    return Container( padding: EdgeInsets.only(left: 10), alignment: Alignment.topLeft, child: Text(snapshot.data!));
-                default: return Center(child: CircularProgressIndicator());
-                }
-              }
-            )
-            ]) : const SizedBox(),
-          ]
-
-      ),
-
-    
-    );
+        )));
   }
 }
 
